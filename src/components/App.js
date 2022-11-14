7095f0712a371e37246c0eeace68181c28bb9cc3
alexbotha <alexbotha@hotmail.co.uk>
2022-11-14 14:56:19 +0000

    updated to v5

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "../App.css";
 import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Football Index</h1>
-      <Home />
+      <Switch>
+        <Route>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,15 @@
 import React from "react";
 
-function Home() {}
+function Home() {
+  return (
+    <div>
+      <h1>Welcome, to the GOAT index</h1>
+      <p>
+        An index for the greatest players to play the game. Search through an
+        array of players, add your own, or even click the cards to review the
+        statistics behind the GOATS
+      </p>
+    </div>
+  );
+}
 export default Home;
