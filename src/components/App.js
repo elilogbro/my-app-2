3da1e824ec340db781f5944a151f9d7243762c3d
alexbotha <alexbotha@hotmail.co.uk>
2022-11-14 15:42:57 +0000

    will add home and player componenet

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import "../App.css";
 import Home from "./Home";
+import playerContainer from "./PlayerContainer";
 
 function App() {
+  const [player, setPlayer] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/playerData")
+      .then((response) => response.json())
+      .then((data) => {
+        {
+          data;
+        }
+      });
+  });
+
   return (
     <div className="App">
       <Switch>
         <Route>
           <Home />
+          <PlayerContainer loading={loading} />
         </Route>
       </Switch>
     </div>
---
+++ src/components/PlayerContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PlayerContainer() {
+  return <div></div>;
+}
+
+export default PlayerContainer;
