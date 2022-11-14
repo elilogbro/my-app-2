d8bf235ea35b4b1114c36bac345cc62efacb7b8a
alexbotha <alexbotha@hotmail.co.uk>
2022-11-14 11:28:24 +0000

    test

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Football Index</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
