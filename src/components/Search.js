06870b8b19c34627d813fe38a90f89c7ff807ae5
alexbotha <alexbotha@hotmail.co.uk>
2022-11-15 18:17:02 +0000

    search component

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+function Search() {
+  return (
+    <div className="search">
+      <h1>Searching for players, below.</h1>
+      <hr></hr>
+      <p>
+        Have a particular player in mind? Want to filter some specific
+        attributes?
+      </p>
+      <p>
+        Search below using the name, age, nationality or position fields to find
+        a tailor made player to your liking.
+      </p>
+    </div>
+  );
+}
+
+export default Search;
